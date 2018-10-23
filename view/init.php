07d81b2b17510e65a73a8a50727cbca2568985dc
Emre Sokullu <esokullu@gmail.com>
2018-10-23 14:09:51 -0700

    update init to reflect new udid dependent host

--- view/init.php
+++ view/init.php
@@ -2,6 +2,7 @@
 <script>
 
 GraphJS.init("<?= esc_attr(get_option(\Graphjs\WordpressPlugin::GRAPHJS_UUID)) ?>", {
+    host: "<?= "https://gj" . strtolower(substr(str_replace("-","", esc_attr(get_option(\Graphjs\WordpressPlugin::GRAPHJS_UUID))), 4)) . ".herokuapp.com" ?>",
     theme: "<?= esc_attr(get_option(\Graphjs\WordpressPlugin::GRAPHJS_THEME)) ?>",
     color: "<?= esc_attr(get_option(\Graphjs\WordpressPlugin::GRAPHJS_COLOR)) ?>",
 });
